9202d621a679dfafe771717bc78e08d31620ac7a
anjali buddhe <anjalibuddhe124@gmail.com>
2023-07-10 21:08:31 +0530

    Add project files.

---
+++ BookCurd/Book.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCurd
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string  author{ get; set; }
+
+    }
+    public class BookCurd
+    {
+        private List<Book> books;
+
+        public BookCurd()
+        {
+            books= new List<Book>();
+        }
+        public void AddBook(Book b)
+        {
+            books.Add(b);
+        }
+        public List<Book> GetAllBooks()
+        {
+            return books;
+        }
+
+        //Console.WriteLine(=========Update==============);
+
+        public void UpdateBook(Book b1)
+        {
+           foreach(Book b in books) 
+            {
+               if(b.Id == b1.Id)
+                {
+                    b.name = b1.name;
+                    b.author=b1.author;
+                }
+            }
+        }
+
+        public void DeleteBook(int id)
+        {
+            foreach(Book b in books)
+            {
+                if(b.Id == id)
+                {
+                    books.Remove(b);
+                    break;
+                }
+            }
+        }
+
+    }
+}
---
+++ BookCurd/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCurd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BookCurd bookCurd = new BookCurd();
+            int option = 0;
+            do
+            {
+                Console.WriteLine("1. Add Book");
+                Console.WriteLine("2. Update Book");
+                Console.WriteLine("3. Delete Book");
+                Console.WriteLine("4. Display Book");
+                int op = Convert.ToInt32(Console.ReadLine());
+
+                switch (op)
+                {
+                    case 1:
+                        //Add Book
+                        Book book = new Book();
+                        Console.WriteLine("Enter Book id");
+                        book.Id = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Book name");
+                        book.name = Console.ReadLine();
+                        Console.WriteLine("Enter author name");
+                        book.author=Console.ReadLine();
+
+                        bookCurd.AddBook(book);
+                        break;
+                    case 2:
+                        //Update
+                        Book book2 = new Book();
+                        Console.WriteLine("Enter id to Modify");
+                        book2.Id = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Book Name");
+                        book2.name=Console.ReadLine();
+                        Console.WriteLine("Enter Author name");
+                        book2.author=Console.ReadLine();
+
+                        bookCurd.UpdateBook(book2);
+
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter id to Delete");
+                        int id = Convert.ToInt32(Console.ReadLine());
+                        bookCurd.DeleteBook(id);
+                        break;
+                    case 4:
+                        //Display all Products
+                        List<Book> books = bookCurd.GetAllBooks();
+                        foreach(Book b in books)
+                        {
+                            Console.WriteLine($"{b.Id}-{b.name}-{b.author}");
+                        }
+                        break;
+                }
+                Console.WriteLine("Press 1 for Continue");
+                option = Convert.ToInt32(Console.ReadLine());
+            }
+            while (option == 1);
+        }
+    }
+}
